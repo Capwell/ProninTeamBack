bd0efd85b4bfff24df37b25277d9e7dec7cd9049
Алексей Шевич <lehashevich@gmail.com>
2022-12-08 09:44:29 +0300

    added cProfiler

--- proninteam/proninteam/settings.py
+++ proninteam/proninteam/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_cprofile_middleware.middleware.ProfilerMiddleware",
     'corsheaders.middleware.CorsMiddleware',
 ]
 
